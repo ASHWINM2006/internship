4103d885a1314744385a9982e2a033ab4fbb3a79
ASHWINM2006 <ashwin20006@gmail.com>
2025-01-23 21:29:31 +0530

    modifi

--- src/FunctionComponent/Navbar.jsx
+++ src/FunctionComponent/Navbar.jsx
@@ -3,41 +3,45 @@ import { Link } from 'react-router-dom';
 import '../CSS/Navbar.css';
 
 const Navbar = () => {
-    const styling = { 
-        textDecoration: "none", 
-        listStyleType: "none", 
-        textAlign: "center"
-    };
     const [dropdown, setDropdown] = useState(false);
-    const toggleDrop = () => {
+
+    const toggleDropdown = () => {
         setDropdown(!dropdown);
     };
 
     return (
         <header>
-            <nav>   
-                <ol style={styling}>
-                    <li><Link to="/" className='link'>Home</Link></li>
-                    <li><Link to="/about" className='link'>About</Link></li>
-                    <li><Link to="/gallery" className='link'>Gallery</Link></li>
-                    <li><Link to="/contact" className='link'>Contact</Link></li>
-                    <div>
-                        <span onMouseEnter={toggleDrop} onMouseLeave={toggleDrop}>Hooks</span>
+            <nav className="navbar">
+                <ul className="navbar-menu">
+                    <li><Link to="/" className="navbar-link">Home</Link></li>
+                    <li><Link to="/about" className="navbar-link">About</Link></li>
+                    <li><Link to="/gallery" className="navbar-link">Gallery</Link></li>
+                    <li><Link to="/contact" className="navbar-link">Contact</Link></li>
+                    <li 
+                        className="dropdown" 
+                        onMouseEnter={toggleDropdown} 
+                        onMouseLeave={toggleDropdown}
+                    >
+                        <span className="dropdown-title">Hooks</span>
                         {dropdown && (
-                            <ul>
-                                <li><Link to="/useState" target='_blank'>useState</Link></li>
-                                <li><Link to="/useEffect" target='_blank'>useEffect</Link></li>
-                                <li><Link to="/useEffectAPI" target='_blank'>useEffectAPI</Link></li>
-                                <li><Link to="/useEffectAPIimage" target='_blank'>useEffectAPIimage</Link></li>
-                                <li><Link to="/useReducer" target='_blank'>useReducer</Link></li>
-                                <li><Link to="/useRef" target='_blank'>useRef</Link></li>
+                            <ul className="dropdown-menu">
+                                <li><Link to="/useState" className="dropdown-link" target="_blank">useState</Link></li>
+                                <li><Link to="/useEffect" className="dropdown-link" target="_blank">useEffect</Link></li>
+                                <li><Link to="/useEffectAPI" className="dropdown-link" target="_blank">useEffectAPI</Link></li>
+                                <li><Link to="/useEffectAPIimage" className="dropdown-link" target="_blank">useEffectAPIimage</Link></li>
+                                <li><Link to="/useReducer" className="dropdown-link" target="_blank">useReducer</Link></li>
+                                <li><Link to="/useRef" className="dropdown-link" target="_blank">useRef</Link></li>
+                                <li><Link to="/useState" className="dropdown-link" target="_blank">UseState</Link></li>
+                                <li><Link to="/useCallback" className="dropdown-link" target="_blank">UseCallback</Link></li>
+                               
+
                             </ul>
                         )}
-                    </div>
-                    <li><Link to="/sign-up" className='link'>Sign-up</Link></li>
-                    <li><Link to="/login" className='link'>Login</Link></li>
-                    <li><Link to="/logout" className='link'>Logout</Link></li>
-                </ol>
+                    </li>
+                    <li><Link to="/sign-up" className="navbar-link">Sign-up</Link></li>
+                    <li><Link to="/login" className="navbar-link">Login</Link></li>
+                    <li><Link to="/logout" className="navbar-link">Logout</Link></li>
+                </ul>
             </nav>
         </header>
     );
